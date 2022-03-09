6f2f18e1916af9513a8efe9bc8ebcc5fd569104a
Olha Vavrychuk <semkivolya@gmail.com>
2022-03-09 19:20:15 +0200

    RankedGradeBook GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,52 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var sortedGrades = (from student in Students
+                                select student.AverageGrade into grade
+                                orderby grade descending
+                                select grade).ToList(); 
+
+            var percentIndexStep = (int)Math.Round(0.2 * sortedGrades.Count);
+
+            int categoryIndexEnd = percentIndexStep - 1;
+            int categoryIndexStart = 0;
+            //in case we have same grades at the end of the range
+            categoryIndexEnd = Math.Max(sortedGrades.LastIndexOf(sortedGrades[categoryIndexEnd]), categoryIndexEnd);
+
+            if (averageGrade >= sortedGrades[categoryIndexEnd])
+                return 'A';
+
+            categoryIndexStart = categoryIndexEnd + 1;
+            categoryIndexEnd += percentIndexStep;
+            if(categoryIndexEnd < sortedGrades.Count)
+                categoryIndexEnd = Math.Max(sortedGrades.LastIndexOf(sortedGrades[categoryIndexEnd]), categoryIndexEnd);
+
+            if(averageGrade >= sortedGrades[categoryIndexEnd] && averageGrade <= sortedGrades[categoryIndexStart])
+                return 'B';
+
+            categoryIndexStart = categoryIndexEnd + 1;
+            categoryIndexEnd += percentIndexStep;
+            if (categoryIndexEnd < sortedGrades.Count)
+                categoryIndexEnd = Math.Max(sortedGrades.LastIndexOf(sortedGrades[categoryIndexEnd]), categoryIndexEnd);
+
+            if (averageGrade >= sortedGrades[categoryIndexEnd] && averageGrade <= sortedGrades[categoryIndexStart])
+                return 'C';
+
+            categoryIndexStart = categoryIndexEnd + 1;
+            categoryIndexEnd += percentIndexStep;
+            if (categoryIndexEnd < sortedGrades.Count)
+                categoryIndexEnd = Math.Max(sortedGrades.LastIndexOf(sortedGrades[categoryIndexEnd]), categoryIndexEnd);
+
+            if (averageGrade >= sortedGrades[categoryIndexEnd] && averageGrade <= sortedGrades[categoryIndexStart])
+                return 'D';
+
+            return 'F';
+        }
     }
 }
